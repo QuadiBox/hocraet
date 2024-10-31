be6ff8784e9bea14e24562f3c66efcd11efb970b
Dojah <oladojaabdquadridamilola@gmail.com>
2024-10-31 23:50:09 +0100

    my commit for changing the version of clerk currently used in the project

--- middleware.js
+++ middleware.js
@@ -9,7 +9,9 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth.protect();
+  if (isProtectedRoute(req)) {
+    auth().protect()
+  } 
 });
 
 export const config = {
